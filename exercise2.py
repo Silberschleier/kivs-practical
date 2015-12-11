07d8073d71949f55fffaf71e185fec176229983a
Christopher Schmidt <christopher.schmidt@posteo.de>
2015-12-11 12:42:05 +0100

    hi

--- exercise2.py
+++ exercise2.py
@@ -4,12 +4,19 @@ import statistics
 import matplotlib.pyplot as plt
 f = open('PA.log', 'r')
 data = []
+timestamp = []
 
 p = re.compile('time=(\d+\.\d+)')
 for line in f:
     m = p.findall(line)
     if m:
         data.append(float(m[0]))
+
+p = re.compile('time=(\d+\\PING)', re.MULTILINE)
+for line in f:
+    m = p.findall(line)
+    if m:
+        timestamp.append(float(m[0]))
 
 print "Minimum: ", min(data)
 print "Maximum: ", max(data)
