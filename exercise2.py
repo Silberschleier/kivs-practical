029a495938d3308ee46ec884053027013fbeb6f9
Christopher Schmidt (Thinkpad) <christopher.schmidt@posteo.de>
2015-12-11 13:21:12 +0100

    Exercise 2.1 fertig

--- exercise2.py
+++ exercise2.py
@@ -2,19 +2,27 @@ import re
 import numpy
 import statistics
 import matplotlib.pyplot as plt
-f = open('PA.log', 'r')
-data = []
+ping_data = []
+timestamp_data = []
 
+contents = ""
+f = open('PA.log', 'r')
+
+t = re.compile('\d+\n')
 p = re.compile('time=(\d+\.\d+)')
 for line in f:
+    if t.match(line) is not None:
+        timestamp = int(line)
     m = p.findall(line)
     if m:
-        data.append(float(m[0]))
+        timestamp += 1
+        timestamp_data.append(timestamp)
+        ping_data.append(float(m[0]))
 
-print "Minimum: ", min(data)
-print "Maximum: ", max(data)
-print "Mean: ", numpy.mean(data)
-print "Deviation: ", statistics.stdev(data)
+print "Minimum: ", min(ping_data)
+print "Maximum: ", max(ping_data)
+print "Mean: ", numpy.mean(ping_data)
+print "Deviation: ", statistics.stdev(ping_data)
 
-plt.plot(data)
+plt.plot(timestamp_data, ping_data, 'bs')
 plt.show()
