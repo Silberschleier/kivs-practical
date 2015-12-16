6457e18e7aa3225b8c45474ea5015c78bba8472c
Christopher Schmidt <christopher.schmidt@posteo.de>
2015-12-16 15:01:56 +0100

    Tex

--- exercise4v2.py
+++ exercise4v2.py
@@ -6,19 +6,19 @@ seconds = []
 datarate = []
 sum = 0
 i = 0
-a = 1000
+length = 1000
 
-while True:
+while True:                                 #Einlesen von stdin in Blöcken der Länge length Bytes
     before = time.time()
-    data = sys.stdin.read(a)
+    data = sys.stdin.read(length)
     seconds.append(time.time() - before)
     if len(data) == 0: break
 
-for s in seconds:
+for s in seconds:                           #Umrechnen von Sekunden pro Bytes in Bytes pro Sekunde
     sum += s
     i += 1
     if sum >= 1:
-        datarate.append(a*i)
+        datarate.append(length*i)
         sum = 0
         i = 0
 
