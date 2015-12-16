947fd144be9c4ec8ef540c850f37bf71242dcf2f
aikira7 <aikira7@web.de>
2015-12-16 15:01:56 +0100

    Tex

--- exercise2.py
+++ exercise2.py
@@ -1,10 +1,9 @@
 import re
-import numpy
 import statistics
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description='bla')
+parser = argparse.ArgumentParser(description='aufgabe2')
 parser.add_argument('--low', type=int, default=0)
 parser.add_argument('--high', type=int, default=10000000000000000)
 args = parser.parse_args()
@@ -14,19 +13,19 @@ timestamp_data = []
 sections = []
 boxplot_data =[]
 timestamp = 0
-c = 500000
+c = 500000                      #Minimaler Abstand zweier Segmente
 i = 0
 
-timeframeLow = args.low
+timeframeLow = args.low         #Zeitfenster, das betrachtet werden soll
 timeframeHigh = args.high
 
 contents = ""
 f = open('PA.log', 'r')
 
-t = re.compile('\d+\n')
-p = re.compile('time=(\d+\.\d+)')
+t = re.compile('\d+\n')                 #Einlesen des Zeitstempels
+p = re.compile('time=(\d+\.\d+)')       #Einlesen der Roundtriptime
 for line in f:
-    if t.match(line) is not None:
+    if t.match(line) is not None:       #Erkennung von Zeitstempeln
         if int(line) > timestamp + c:
             sections.append(i)
         timestamp = int(line)
@@ -38,22 +37,17 @@ for line in f:
             ping_data.append(float(m[0]))
             i += 1
 
-for s in range(1, len(sections)):
+for s in range(1, len(sections)):                                #Auswertung der Pingdaten
     if (s) > len(sections):
         boxplot_data.append(ping_data[sections[s]:])
     else:
         boxplot_data.append(ping_data[sections[s-1]:sections[s]])
 print "Minimum: ", min(ping_data)
 print "Maximum: ", max(ping_data)
-print "Mean: ", numpy.mean(ping_data)
+print "Mean: ", statistics.mean(ping_data)
 print "Deviation: ", statistics.stdev(ping_data)
 
-print len(ping_data)
-
-print sections
-
-#print boxplot_data[2]
-
+print "Sections: ", sections
 
 plt.plot(timestamp_data, ping_data, 'bs')
 plt.savefig("timestamp_data.png")
